11bbe01c545254091327302a6a6482cf50e472ba
Karim Zaoui <kzaoui@made.com>
2019-03-13 22:27:12 +0000

    added src folder

---
+++ src/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py_tdd',
+    url='https://github.com/KarimZaoui/py_tdd',
+    description='Python TDD',
+    packages=find_packages('.'),
+)
